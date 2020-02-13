f549134fc1b77291b79c4beacb02f2d03b72b7d9
zhaoqiangcn <zq19890000@gmail.com>
2020-02-13 15:15:07 +0800

    rebuilding site 2020年02月13日 15:15:07

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project.
+hugo -t loveit
+
+#echo "acbdc102e2c95f016d423fa03690923d $2"
+#searchKey="$2"
+#python2 search_process.py -k "${searchKey}" || fail "site search data process fail. Error Code: [ $? ]"
+
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin develop
+
